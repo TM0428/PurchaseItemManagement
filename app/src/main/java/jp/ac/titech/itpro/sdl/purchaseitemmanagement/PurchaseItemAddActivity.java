25d0367b3fb42c62aa127ebd9573f857ebc88ae5
t.m <takashi.orion.999.428@gmail.com>
2022-06-30 21:31:04 +0900

    カメラが起動できるように変更

--- app/src/main/java/jp/ac/titech/itpro/sdl/purchaseitemmanagement/PurchaseItemAddActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/purchaseitemmanagement/PurchaseItemAddActivity.java
@@ -1,24 +1,56 @@
 package jp.ac.titech.itpro.sdl.purchaseitemmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
+import jp.ac.titech.itpro.sdl.purchaseitemmanagement.databinding.ActivityPurchaseItemAddBinding;
 
 public class PurchaseItemAddActivity extends AppCompatActivity {
     private Uri photoImage = null;
     private String currentPhotoPath;
+    private ActivityPurchaseItemAddBinding mBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_purchase_item_add);
+        mBinding = ActivityPurchaseItemAddBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+        mBinding.btCamera.setOnClickListener(v -> {
+            try {
+                Log.d("TM","photo button push");
+                File file = createImageFile();
+                photoImage = FileProvider.getUriForFile(
+                        PurchaseItemAddActivity.this,
+                        getApplicationContext().getPackageName() + ".fileprovider",
+                        file
+                );
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                // TODO: You should setup appropriate parameters for the intent
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoImage);
+                startActivity(intent);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
 
